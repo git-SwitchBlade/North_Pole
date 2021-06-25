6f6c363a76e4cc42948bb7d68961d8173dcf802b
$witchBlade <85271575+git-SwitchBlade@users.noreply.github.com>
2021-06-25 23:29:08 +0530

    Update wifi_catcher.py

--- wifi_catcher.py
+++ wifi_catcher.py
@@ -1,13 +1,17 @@
-import subprocess
 import platform
+import os
 
 
 identify = platform.system()
 
 if identify == "Windows":
     print(f"Your Operating Platform Is {identify}")
-    networks = subprocess.check_output(["netsh", "wlan", "show", "networks","mode=BSSID"])
-    decode = networks.decode('ascii')
-    print(decode)
+    os.system("netsh wlan show networks mode=BSSID")
+
+elif identify == "Linux":
+    print(f"Your Operating Platform Is {identify}")
+    os.system("nmcli -f SSID,BSSID,ACTIVE dev wifi list")
+
+
 else:
     print("Unsupported For Your Operating System/\.")
