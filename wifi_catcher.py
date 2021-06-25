2af7b2c996cc2656daa6d0e84938c771f4912111
$witchBlade <85271575+git-SwitchBlade@users.noreply.github.com>
2021-06-26 00:14:02 +0530

    Update wifi_catcher.py

--- wifi_catcher.py
+++ wifi_catcher.py
@@ -1,17 +1,30 @@
+import subprocess
 import platform
-import os
-
 
 identify = platform.system()
 
-if identify == "Windows":
-    print(f"Your Operating Platform Is {identify}")
-    os.system("netsh wlan show networks mode=BSSID")
+print(f"Your Operating Platform Is {identify}")
+
+if identify  == "Windows":
+    cmd = ["netsh", "wlan", "show", "networks", "mode=BSSID"]
+    networks = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    output, errors = networks.communicate()
+    print(output.decode("utf-8"))
+    data = output.decode("utf-8")
+    f = open("output.txt", "a+")
+    f.write(data)
+    f.write("\n----------------------------------------------------------------------------")
+    f.close()
 
 elif identify == "Linux":
-    print(f"Your Operating Platform Is {identify}")
-    os.system("nmcli -f SSID,BSSID,ACTIVE dev wifi list")
-
-
+    cmd = ["nmcli", "-f",  "SSID,BSSID,ACTIVE", "dev", "wifi", "list"] 
+    networks = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    output, errors = networks.communicate()
+    print(output.decode("utf-8"))
+    data = output.decode("utf-8")
+    f = open("output.txt", "a+")
+    f.write(data)
+    f.write("\n----------------------------------------------------------------------------")
+    f.close()
 else:
     print("Unsupported For Your Operating System/\.")
